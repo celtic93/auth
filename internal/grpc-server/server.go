3d726623bf148e1949f5c9f9957f27984186ad38
celtic93 <egorka80@list.ru>
2025-03-06 14:18:11 +0300

    Fixed linter errors #1

--- internal/grpc-server/server.go
+++ internal/grpc-server/server.go
@@ -16,21 +16,21 @@ type Server struct {
 }
 
 // Get: gets user by ID
-func (s *Server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+func (s *Server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
 	log.Printf("server.Get User id: %d", req.GetId())
 
 	return &desc.GetResponse{
-		Id: req.GetId(),
-		Name: gofakeit.Name(),
-		Email: gofakeit.Email(),
-		Role: desc.Role_USER,
+		Id:        req.GetId(),
+		Name:      gofakeit.Name(),
+		Email:     gofakeit.Email(),
+		Role:      desc.Role_USER,
 		CreatedAt: timestamppb.New(gofakeit.Date()),
 		UpdatedAt: timestamppb.New(gofakeit.Date()),
 	}, nil
 }
 
 // Create: creates user
-func (s *Server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+func (s *Server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	log.Printf("server.Create User id: %s", req.GetEmail())
 
 	return &desc.CreateResponse{
@@ -39,14 +39,14 @@ func (s *Server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.Cre
 }
 
 // Create: creates user
-func (s *Server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+func (s *Server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
 	log.Printf("server.Update User id: %d", req.GetId())
 
 	return &emptypb.Empty{}, nil
 }
 
 // Create: creates user
-func (s *Server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+func (s *Server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
 	log.Printf("server.Delete User id: %d", req.GetId())
 
 	return &emptypb.Empty{}, nil
